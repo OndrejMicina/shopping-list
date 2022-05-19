61b0da6284fa4d47bf7e626e1ec15858f79cfc59
Ondrej Mičina <ondrej.micina@gmail.com>
2022-05-19 16:53:52 +0200

    Created first template

---
+++ shopping_list/lib/models/GroceryModel.dart
@@ -0,0 +1,8 @@
+class GroceryModel {
+  String groceryName;
+  double ammount;
+  bool isSelected;
+  String addedBy;
+
+  GroceryModel(this.groceryName, this.ammount, this.addedBy, this.isSelected);
+}
